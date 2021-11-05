6200f7e3e84fbef2bd8cdf4513d9d2fabfc31705
Pranav Vohra <pranavvohra1812@yahoo.in>
2021-11-05 22:56:59 +0530

    first change for ci/cd

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Azure Playground <button>Click Me</button>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://portal.azure.com/#home"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          First Change
         </a>
       </header>
     </div>
